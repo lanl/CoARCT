548fd91ee0d6692626aec6abb3b63fbe915d73e2
Timothy Kelley <tkelley@lanl.gov>
2017-01-03 12:42:05 -0700

    Initial commit, unit tests for utilities.h.

---
+++ test/lib/clang_utilities_test.cc
@@ -0,0 +1,119 @@
+// clang_utilities_test.cc
+// T. M. Kelley
+// Jan 03, 2017
+// (c) Copyright 2017 LANSLLC, all rights reserved
+
+/* Tests of utilities that build on clang */
+#include "utilities.h"
+#include "gtest/gtest.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/Refactoring.h"
+#include "clang/Frontend/FrontendActions.h"
+#include <tuple>
+
+using namespace corct;
+using namespace clang;
+using namespace clang::ast_matchers;
+
+/* Test strategy: write code including element to be tested, such as member
+expression; compile to AST; use matcher to extract interesting node; apply
+function to interesting node. It's a lot of work to get around constructing a
+consistent AST subtree directly.
+*/
+
+using ASTUPtr = std::unique_ptr<ASTUnit>;
+
+/**\brief Compile code fragment to AST.
+\param code: valid c++ code
+\return {unique_ptr<AST>,ASTContext *, TranslationDecl *}
+*/
+inline auto prep_code(str_t_cr code) {
+  ASTUPtr ast(clang::tooling::buildASTFromCode(code));
+  ASTContext* pctx = &(ast->getASTContext());
+  TranslationUnitDecl* decl = pctx->getTranslationUnitDecl();
+  return std::make_tuple(std::move(ast), pctx, decl);
+}
+
+// is_on_lhs:
+struct Tests_On_LHS : public callback_t {
+  void run(result_t const& result) override {
+    MemberExpr const* mexpr = result.Nodes.getNodeAs<MemberExpr>("mexpr");
+    ASTContext* pctx = result.Context;
+    if (mexpr) {
+      actually_on_lhs_ = is_on_lhs(mexpr, *pctx);
+    }
+    return;
+  }  // run
+  bool actually_on_lhs_ = false;
+};
+
+bool run_case_is_on_lhs(str_t_cr code) {
+  ASTUPtr ast;
+  ASTContext* pctx;
+  TranslationUnitDecl* decl;
+  std::tie(ast, pctx, decl) = prep_code(code);
+  Tests_On_LHS tol;
+  finder_t finder;
+  DeclarationMatcher m(hasDescendant(memberExpr().bind("mexpr")));
+  finder.addMatcher(m, &tol);
+  finder.match(*decl, *pctx);
+  return tol.actually_on_lhs_;
+}  // run_case_is_on_lhs
+
+TEST(utilities, on_lhs_case1) {
+  // some code that includes exactly one member reference, and on the LHS
+  string_t const code = "struct S{  int i;};void f(){  S s = {1};  s.i = 2;}";
+  bool on_lhs = run_case_is_on_lhs(code);
+  EXPECT_TRUE(on_lhs);
+}  // TEST(utilities,on_lhs){
+
+TEST(utilities, on_lhs_case2) {
+  // some code that includes exactly one member reference, but on the RHS
+  string_t const code = "struct S{  int i;};void f(){  S s = {1};int i = s.i;}";
+  bool on_lhs = run_case_is_on_lhs(code);
+  EXPECT_FALSE(on_lhs);
+}  // TEST(utilities,on_lhs){
+
+// is_part_of_assignment:
+struct Tests_PartOfAssgnmt : public callback_t {
+  void run(result_t const& result) override {
+    MemberExpr const* mexpr = result.Nodes.getNodeAs<MemberExpr>("mexpr");
+    ASTContext* pctx = result.Context;
+    if (mexpr) {
+      actually_part_ = is_part_of_assignment(mexpr, *pctx);
+    }
+    return;
+  }  // run
+  bool actually_part_ = false;
+};
+
+bool run_case_part_of_assignment(str_t_cr code) {
+  ASTUPtr ast;
+  ASTContext* pctx;
+  TranslationUnitDecl* decl;
+  std::tie(ast, pctx, decl) = prep_code(code);
+  Tests_PartOfAssgnmt tpoa;
+  finder_t finder;
+  DeclarationMatcher m(hasDescendant(memberExpr().bind("mexpr")));
+  finder.addMatcher(m, &tpoa);
+  finder.match(*decl, *pctx);
+  return tpoa.actually_part_;
+}
+
+TEST(utilities, is_part_of_assignment_case1) {
+  // code includes exactly one member reference, and is part of assignment
+  string_t const code =
+      "struct S{ int i;};void g(int){return;}void f(){S s;s.i = 3;}";
+  bool is_part(run_case_part_of_assignment(code));
+  EXPECT_TRUE(is_part);
+}
+
+TEST(utilities, is_part_of_assignment_case2) {
+  // code includes exactly one member reference, and is not part of assignment
+  string_t const code =
+      "struct S{ int i;};void g(int){return;}void f(){ S s; g(s.i);}";
+  bool is_part(run_case_part_of_assignment(code));
+  EXPECT_FALSE(is_part);
+}
+
+// End of file
---
+++ test/lib/utilities_test.cc
@@ -0,0 +1,78 @@
+// utilities_test.cc
+// T. M. Kelley
+// Oct 13, 2016
+// (c) Copyright 2016 LANSLLC, all rights reserved
+
+#include "types.h"
+#include "utilities.h"
+#include "gtest/gtest.h"
+#include <iostream>
+#include <sstream>
+
+namespace corct {
+void calls_here()  // DO NOT MOVE THIS EVER!! (or fix the line # in the test.)
+{
+  HERE("hola, testy amigos!");
+  return;
+}
+
+TEST(utilities, here) {
+  std::stringstream s;
+  std::streambuf* orig_buf = std::cout.rdbuf(s.rdbuf());
+  calls_here();
+  // restore cout
+  std::cout.rdbuf(orig_buf);
+  string_t exp_str = "calls_here:15: hola, testy amigos!\n";
+  EXPECT_EQ(exp_str, s.str());
+}  // TEST(utilities, here)
+
+TEST(utilities, add_tab) {
+  string_t const s1("ooga");
+  string_t const s2 = add_tab(s1);
+  EXPECT_TRUE(s2 == (s1 + '\t'));
+}
+
+TEST(utilities, remove_tab) {
+  string_t const s1("ooga\t");
+  string_t const s2 = remove_tab(s1);
+  EXPECT_TRUE(s2 == "ooga");
+}
+
+TEST(utilities, in_vec) {
+  using corct::in_vec;
+  {
+    using vec_t = std::vector<int>;
+    vec_t v1 = {1, 2, 3};
+    EXPECT_TRUE(in_vec(v1, 1));
+    EXPECT_FALSE(in_vec(v1, 4));
+  }
+  {
+    using vec_t = std::vector<double>;
+    vec_t v1 = {1, 2, 3};
+    EXPECT_TRUE(in_vec(v1, 1));
+    EXPECT_FALSE(in_vec(v1, 4));
+  }
+}  // TEST(utilities,in_vec)
+
+TEST(utilities, split) {
+  string_t s = "foo,bar,bumz,bibz";
+  vec_str ss = split(s, ',');
+  vec_str ss_exp = {"foo", "bar", "bumz", "bibz"};
+  bool const ok = std::equal(ss.begin(), ss.end(), ss_exp.begin());
+  EXPECT_TRUE(ok);
+}
+
+TEST(utilities, check_ptr) {
+  std::stringstream s;
+  std::streambuf* orig_buf = std::cout.rdbuf(s.rdbuf());
+  check_ptr<double>(nullptr,"bob","  ");
+  // restore cout
+  std::cout.rdbuf(orig_buf);
+  string_t exp_str = "  Invalid pointer bob\n";
+  EXPECT_EQ(exp_str, s.str());
+}  // TEST(utilities, check_ptr)
+
+
+} // corct::
+
+// End of file
