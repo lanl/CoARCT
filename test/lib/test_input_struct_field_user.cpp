113ecad37aae4eb7ff9062154b6d49c73135b625
Timothy Kelley <tkelley@lanl.gov>
2017-01-06 16:44:11 -0700

    Test code for struct_field_user

---
+++ test/lib/test_input_struct_field_user.cpp
@@ -0,0 +1,20 @@
+// test_input_struct_field_user.cc
+// T. M. Kelley
+// Jan 06, 2017
+// (c) Copyright 2017 LANSLLC, all rights reserved
+
+struct foo_t{
+  int i;
+};
+struct bar_t{
+  double k;
+};
+void f1(foo_t & f){
+  f.i = 16;
+}
+void f2(bar_t & b){
+  b.k = 3.14;
+}
+// struct foo_t{int i;};struct bar_t{double k;};void f1(foo_t & f){f.i = 16;}void f2(bar_t & b){b.k = 3.14;}
+
+// End of file
