ad2bd36a966acef2978089a4cdad33cfd6341bda
Timothy Kelley <tkelley@lanl.gov>
2017-01-18 13:04:25 -0700

    Formatting, comments

--- apps/GlobalDetect.cc
+++ apps/GlobalDetect.cc
@@ -14,8 +14,8 @@ using namespace llvm;
 using namespace clang::ast_matchers;
 
 static cl::extrahelp MoreHelp(
-    "\nReport all functions that use global variable, or all sites in which "
-    "global variables  are used");
+    "\nReport all functions that use global variable, or all sites at which "
+    "global variables are used");
 
 // command line options:
 static llvm::cl::OptionCategory GDOpts("global-detect options");
@@ -27,14 +27,9 @@ static cl::opt<std::string> old_var_string(
     cl::cat(GDOpts),
     cl::init(""));
 
-static cl::opt<bool> dry_run("d",
-                             cl::desc("dry run"),
-                             cl::cat(GDOpts),
-                             cl::init(false));
-
 static cl::opt<bool> report_functions(
     "f",
-    cl::desc("just report functions that use the target global"),
+    cl::desc("just report functions that use the target global(s)"),
     cl::cat(GDOpts),
     cl::init(false));
 
@@ -48,13 +43,11 @@ main(int argc, const char ** argv)
 
   Global_Printer printer(std::cout);
   StatementMatcher global_var_matcher =
-      (old_var_string == "")
-          ? all_global_var_matcher()
-          : mk_global_var_matcher(old_var_string);
+      (old_var_string == "") ? all_global_var_matcher()
+                             : mk_global_var_matcher(old_var_string);
   DeclarationMatcher global_func_matcher =
-      (old_var_string == "")
-          ? all_global_fn_matcher()
-          : mk_global_fn_matcher(old_var_string);
+      (old_var_string == "") ? all_global_fn_matcher()
+                             : mk_global_fn_matcher(old_var_string);
 
   clang::ast_matchers::MatchFinder finder;
   if(report_functions) {
