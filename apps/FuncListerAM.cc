def93662e7516000ab5d25ff3e949445c3b7aa33
Timothy Kelley <tkelley@lanl.gov>
2017-01-31 15:53:55 -0700

    Formatting, updated CMakeLists to both RAV- & matchers-based apps.

--- apps/FuncListerAM.cc
+++ apps/FuncListerAM.cc
@@ -7,9 +7,9 @@
  * skipping the ones that are defined in headers. Compare with FuncListerRAV,
  * which uses RecursiveASTVisitor. */
 
-#include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 #include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 #include <iostream>
@@ -18,65 +18,82 @@ using namespace clang::tooling;
 using namespace llvm;
 using namespace clang::ast_matchers;
 
-uint32_t n_matches = 0;
-uint32_t n_skipped_matches = 0;
+uint32_t num_funcs = 0;
+uint32_t num_skipped_funcs = 0;
 
-void print_func(clang::FunctionDecl const * const fdecl){
+void
+print_func(clang::FunctionDecl const * const fdecl)
+{
   std::cout << "Function '" << (fdecl->getNameAsString()) << "' defined\n";
   fdecl->dump();
   return;
-} // print_func
+}  // print_func
 
-auto mk_fn_decl_matcher(){
+auto
+mk_fn_decl_matcher()
+{
   return functionDecl(isExpansionInMainFile()).bind("fdecl");
 }
 
-struct FuncPrinter : public MatchFinder::MatchCallback{
-  void run(MatchFinder::MatchResult const & result) override {
+struct FuncPrinter : public MatchFinder::MatchCallback {
+  void run(MatchFinder::MatchResult const & result) override
+  {
     using namespace clang;
-    FunctionDecl const * fdecl =
-        result.Nodes.getNodeAs<FunctionDecl>("fdecl");
-    if(fdecl){
-      n_matches++;
+    FunctionDecl const * fdecl = result.Nodes.getNodeAs<FunctionDecl>("fdecl");
+    if(fdecl) {
+      num_funcs++;
       print_func(fdecl);
     }
-    else { std::cerr << "Invalid fdecl\n";}
+    else {
+      std::cerr << "Invalid fdecl\n";
+    }
     return;
-  } // run
-}; // FuncPrinter
+  }  // run
 
-auto mk_fn_skipper_matcher(){
+};  // FuncPrinter
+
+/* The RAV approach also makes a note when the analysis skips a function
+* (because it's not part of the main file). Here is an AST Matcher way of
+* accomplishing the same thing. */
+auto
+mk_fn_skipper_matcher()
+{
   return functionDecl(unless(isExpansionInMainFile())).bind("fdecl");
 }
-struct FuncSkipper : public MatchFinder::MatchCallback{
-  void run(MatchFinder::MatchResult const & result) override {
+
+struct FuncSkipper : public MatchFinder::MatchCallback {
+  void run(MatchFinder::MatchResult const & result) override
+  {
     using namespace clang;
-    FunctionDecl const * fdecl =
-        result.Nodes.getNodeAs<FunctionDecl>("fdecl");
-    if(fdecl){
+    FunctionDecl const * fdecl = result.Nodes.getNodeAs<FunctionDecl>("fdecl");
+    if(fdecl) {
       std::cout << "Skipping " << fdecl->getNameAsString()
                 << " not from target file\n";
-      n_skipped_matches++;
+      num_skipped_funcs++;
     }
-    else { std::cerr << "Invalid fdecl\n";}
+    else {
+      std::cerr << "Invalid fdecl\n";
+    }
     return;
-  } // run
-}; // FuncSkipper
+  }  // run
+};   // FuncSkipper
 
 static llvm::cl::OptionCategory flt_cat("func-decl-list-am options");
 
-int main(int argc, const char **argv){
+int
+main(int argc, const char ** argv)
+{
   CommonOptionsParser OptionsParser(argc, argv, flt_cat);
   ClangTool Tool(OptionsParser.getCompilations(),
                  OptionsParser.getSourcePathList());
   FuncPrinter fp;
   FuncSkipper fs;
   MatchFinder finder;
-  finder.addMatcher(mk_fn_decl_matcher(),&fp);
-  finder.addMatcher(mk_fn_skipper_matcher(),&fs);
+  finder.addMatcher(mk_fn_decl_matcher(), &fp);
+  finder.addMatcher(mk_fn_skipper_matcher(), &fs);
   int rslt = Tool.run(newFrontendActionFactory(&finder).get());
-  std::cout << "Reported " << n_matches << " functions\n";
-  std::cout << "Skipped " << n_skipped_matches << " functions\n";
+  std::cout << "Reported " << num_funcs << " functions\n";
+  std::cout << "Skipped " << num_skipped_funcs << " functions\n";
   return rslt;
 }
 
--- apps/FuncListerRAV.cc
+++ apps/FuncListerRAV.cc
@@ -6,77 +6,81 @@
 /* Uses RecrusiveASTVisitor to list all the functions in a translation unit,
  * skipping the ones that are defined in headers. */
 
-#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/AST/ASTConsumer.h"
+#include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Frontend/CompilerInstance.h"
 #include "clang/Frontend/FrontendAction.h"
-#include "clang/Tooling/Tooling.h"
 #include "clang/Tooling/CommonOptionsParser.h"
+#include "clang/Tooling/Tooling.h"
 
 #include <iostream>
 
 uint32_t num_funcs = 0;
 uint32_t num_skipped_funcs = 0;
 
+void print_func(clang::FunctionDecl * fdecl)
+{
+  std::cout << "Function '" << (fdecl->getNameAsString()) << "' defined\n";
+  fdecl->dump();
+  return;
+}  // print_func
+
 class FunctionLister : public clang::RecursiveASTVisitor<FunctionLister> {
 public:
-  clang::ASTContext * ast_ctx;
+  clang::ASTContext * ast_ctx_;
 
   explicit FunctionLister(clang::CompilerInstance * ci)
-    : ast_ctx(&(ci->getASTContext())) {}
-
-  void print_func(clang::FunctionDecl * fdecl){
-    std::cout << "Function '" << (fdecl->getNameAsString()) << "' defined\n";
-    fdecl->dump();
-    return;
-  } // print_func
+      : ast_ctx_(&(ci->getASTContext() ) ) {}
 
   bool VisitFunctionDecl(clang::FunctionDecl * fdecl)
   {
-    clang::SourceManager & sm(ast_ctx->getSourceManager());
+    clang::SourceManager & sm(ast_ctx_->getSourceManager());
     // cf cfe-3.9.0.src/include/clang/ASTMatchers/ASTMatcher.h:209-214
     bool const inMainFile(
         sm.isInMainFile(sm.getExpansionLoc(fdecl->getLocStart())));
-    if(inMainFile){
+    if(inMainFile) {
       num_funcs++;
       print_func(fdecl);
     }
-    else{
+    else {
       std::cout << "Skipping " << fdecl->getNameAsString()
                 << " not from target file\n";
       num_skipped_funcs++;
     }
     return true;
-  } // VisitFunctionDecl
-}; // FunctionLister
+  }  // VisitFunctionDecl
+};   // FunctionLister
 
 class FunctionListerConsumer : public clang::ASTConsumer {
 public:
-  virtual void HandleTranslationUnit(clang::ASTContext &ctx){
+  virtual void HandleTranslationUnit(clang::ASTContext & ctx)
+  {
     lister_.TraverseDecl(ctx.getTranslationUnitDecl());
   }
 
-  explicit FunctionListerConsumer(clang::CompilerInstance *ci)
-  : lister_(ci){}
-
+  explicit FunctionListerConsumer(clang::CompilerInstance * ci) : lister_(ci) {}
 private:
   FunctionLister lister_;
-}; // FunctionListerConsumer
+};  // FunctionListerConsumer
 
-class FuncListerAction : public clang::ASTFrontendAction{
+class FuncListerAction : public clang::ASTFrontendAction {
 public:
   virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(
-    clang::CompilerInstance & ci, llvm::StringRef file){
+      clang::CompilerInstance & ci,
+      llvm::StringRef file)
+  {
     return std::unique_ptr<clang::ASTConsumer>(new FunctionListerConsumer(&ci));
   }
-}; // FuncListerAction
+};  // FuncListerAction
 
 static llvm::cl::OptionCategory flt_cat("func-decl-list options");
 
-int main(int argc, const char ** argv){
+int
+main(int argc, const char ** argv)
+{
   using namespace clang::tooling;
-  CommonOptionsParser op(argc,argv,flt_cat);
-  ClangTool tool(op.getCompilations(),op.getSourcePathList());
+  CommonOptionsParser op(argc, argv, flt_cat);
+  ClangTool tool(op.getCompilations(), op.getSourcePathList());
   int result = tool.run(newFrontendActionFactory<FuncListerAction>().get());
   std::cout << "Reported " << num_funcs << " functions\n";
   std::cout << "Skipped " << num_skipped_funcs << " functions\n";
