139faa9b72c43fe2684de2a350d0113489cfa680
Timothy Kelley <tkelley@lanl.gov>
2017-02-08 14:53:25 -0800

    Extracted common test code

--- test/lib/callsite_expander_test.cc
+++ test/lib/callsite_expander_test.cc
@@ -1,4 +1,4 @@
-// func_matchers_test.cc
+// callsite_expander_test.cc
 // T. M. Kelley
 // Feb 06, 2017
 // (c) Copyright 2017 LANSLLC, all rights reserved
@@ -199,158 +199,135 @@ TEST(mthd_call_matcher, case5_distinguishesBoundVsFree)
   EXPECT_EQ(exp_matches,run_case<Testr2>(code, tst));
 }
 
-// Now test the full expand_callsite class
+string_t const fname = "input.cc";
+string_t const cboo = ", boo";
+string_t const boo = "boo";
+string_t const new_arg = "boo";
+
+using EC = expand_callsite;
+using replacements_t = EC::Base::replacements_t;
+using reps_it = replacements_t::iterator;
+
+template <typename Tester>
+bool run_case_ec(string_t const & code, Tester & t, replacements_t const & reps_exp){
+  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
+  std::tie(ast, pctx, decl) = prep_code(code);
+  auto ms(t.matchers());
+  finder_t finder;
+  for(auto & m: ms){
+    finder.addMatcher(m, &t);
+  }
+  finder.matchAST(*pctx);
+  replacements_t const & reps(t.get_replacements());
+  bool const size_ok(reps_exp.size() == reps.size());
+  EXPECT_EQ(reps_exp.size(),reps.size());
+  bool const reps_match = reps_exp == reps;
+  if(!reps_match){
+    std::cout << "reps disagree:\n";
+    reps_it ri = reps.begin(), ei = reps_exp.begin();
+    for(; ri != reps.end() && ei != reps_exp.end(); ++ri, ++ei){
+      std::cout << "expected: " << ei->toString() << ", actual: "
+        << ri->toString()<< "\n";
+    }
+  }
+  EXPECT_EQ(reps_exp,reps);
+  return size_ok && reps_match;
+} // run_case_ec
+
 TEST(expand_callsite,case1_expands)
 {
   string_t const code = "void f(int){return;} void g(){int i(42);f(i); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
   expand_callsite::Base::replacements_t reps; // in 3.9, this is std::set
   vec_str targs = {"f"};
-  string_t new_arg("boo");
   expand_callsite ec(reps,targs,new_arg,false);
-  auto ms(ec.matchers());
-  finder_t finder;
-  for(auto & m: ms){
-    finder.addMatcher(m, &ec);
-  }
-  finder.matchAST(*pctx);
-  EXPECT_EQ(1u,reps.size());
-  replacement_t const & rep1(*reps.begin());
-  EXPECT_EQ("input.cc",rep1.getFilePath());
-  EXPECT_EQ(43u,rep1.getOffset());
-  EXPECT_EQ(0u,rep1.getLength());
-  EXPECT_EQ(", boo",rep1.getReplacementText());
-} // TEST(expand_callsite,expands)
+  replacements_t exp_r = {{fname,43u,0u,cboo}};
+  run_case_ec<EC>(code,ec,exp_r);
+}
+
 // case 2: no parameters in function
 TEST(expand_callsite,case2_expands)
 {
   string_t const code = "void f(){return;} void g(){f(); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
-  expand_callsite::Base::replacements_t reps; // in 3.9, this is std::set
+  // ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
+  // std::tie(ast, pctx, decl) = prep_code(code);
+  replacements_t reps; // in 3.9, this is std::set
   vec_str targs = {"f"};
-  string_t new_arg("boo");
   expand_callsite ec(reps,targs,new_arg,false);
-  auto ms(ec.matchers());
-  finder_t finder;
-  for(auto & m: ms){
-    finder.addMatcher(m, &ec);
-  }
-  finder.matchAST(*pctx);
-  EXPECT_EQ(1u,reps.size());
-  replacement_t const & rep1(*reps.begin());
-  EXPECT_EQ("input.cc",rep1.getFilePath());
-  EXPECT_EQ(29u,rep1.getOffset());
-  EXPECT_EQ(0u,rep1.getLength());
-  EXPECT_EQ("boo",rep1.getReplacementText());
+  // auto ms(ec.matchers());
+  // finder_t finder;
+  // for(auto & m: ms){
+  //   finder.addMatcher(m, &ec);
+  // }
+  // finder.matchAST(*pctx);
+  // EXPECT_EQ(1u,reps.size());
+  // replacements_t const & rep1(*reps.begin());
+  replacements_t exp_reps = {{fname,29u,0u,boo}};
+  run_case_ec(code,ec,exp_reps);
+  // EXPECT_EQ("input.cc",rep1.getFilePath());
+  // EXPECT_EQ(29u,rep1.getOffset());
+  // EXPECT_EQ(0u,rep1.getLength());
+  // EXPECT_EQ("boo",rep1.getReplacementText());
 } // TEST(expand_callsite,expands)
 // case 3: has a defaulted parameter in first spot (un-overridden in call)
 TEST(expand_callsite,case3_expands)
 {
   string_t const code = "void f(int i = 42){return;} void g(){f(); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
   expand_callsite::Base::replacements_t reps; // in 3.9, this is std::set
   vec_str targs = {"f"};
-  string_t new_arg("boo");
-  bool const verbose(false);
-  expand_callsite ec(reps,targs,new_arg,false,verbose);
-  auto ms(ec.matchers());
-  finder_t finder;
-  for(auto & m: ms){
-    finder.addMatcher(m, &ec);
-  }
-  finder.matchAST(*pctx);
-  EXPECT_EQ(1u,reps.size());
-  replacement_t const & rep1(*reps.begin());
-  EXPECT_EQ("input.cc",rep1.getFilePath());
-  EXPECT_EQ(38u,rep1.getOffset());
-  EXPECT_EQ(0u,rep1.getLength());
-  EXPECT_EQ("boo",rep1.getReplacementText());
+  expand_callsite ec(reps,targs,new_arg,false);
+  replacements_t exp_reps = {{fname,38u,0u,boo}};
+  run_case_ec(code,ec,exp_reps);
 } // TEST(expand_callsite,expands)
 // case 4: has a defaulted parameter in first spot (overridden in call)
 TEST(expand_callsite,case4_expands)
 {
   string_t const code = "void f(int i = 42){return;} void g(){int i(43);f(i); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
   expand_callsite::Base::replacements_t reps; // in 3.9, this is std::set
   vec_str targs = {"f"};
-  string_t new_arg("boo");
-  bool const verbose(false);
-  expand_callsite ec(reps,targs,new_arg,false,verbose);
-  auto ms(ec.matchers());
-  finder_t finder;
-  for(auto & m: ms){
-    finder.addMatcher(m, &ec);
-  }
-  finder.matchAST(*pctx);
-  EXPECT_EQ(1u,reps.size());
-  replacement_t const & rep1(*reps.begin());
-  EXPECT_EQ("input.cc",rep1.getFilePath());
-  EXPECT_EQ(48u,rep1.getOffset());
-  EXPECT_EQ(0u,rep1.getLength());
-  EXPECT_EQ("boo",rep1.getReplacementText());
+  expand_callsite ec(reps,targs,new_arg,false);
+  replacements_t exp_reps = {{fname,48u,0u,boo}};
+  run_case_ec(code,ec,exp_reps);
 } // TEST(expand_callsite,expands)
 // case 5: defaulted parameter after first spot (not overridden in call)
 TEST(expand_callsite,case5_expands)
 {
   string_t const code = "void f(double d, int i = 42){return;} void g(){f(3.14159); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
   expand_callsite::Base::replacements_t reps; // in 3.9, this is std::set
   vec_str targs = {"f"};
-  string_t new_arg("boo");
-  bool const verbose(false);
-  expand_callsite ec(reps,targs,new_arg,false,verbose);
-  auto ms(ec.matchers());
-  finder_t finder;
-  for(auto & m: ms){
-    finder.addMatcher(m, &ec);
-  }
-  finder.matchAST(*pctx);
-  EXPECT_EQ(1u,reps.size());
-  replacement_t const & rep1(*reps.begin());
-  EXPECT_EQ("input.cc",rep1.getFilePath());
-  EXPECT_EQ(56u,rep1.getOffset());
-  EXPECT_EQ(0u,rep1.getLength());
-  EXPECT_EQ(", boo",rep1.getReplacementText());
+  expand_callsite ec(reps,targs,new_arg,false);
+  replacements_t exp_reps = {{fname,56u,0u,cboo}};
+  run_case_ec(code,ec,exp_reps);
 } // TEST(expand_callsite,expands)
+/* This test checks both function and methods matching using the
+same Callback. This currently requires manually registering the method names.*/
 TEST(expand_callsite,case6_method_expands)
 {
-  using EC = expand_callsite;
   string_t const code =
     "void f(){return;} void g(){f(); return;} struct S{  void h(){return;}  void i(){g();}};void k(S & s){ s.h(); return;}";
-  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
-  std::tie(ast, pctx, decl) = prep_code(code);
   EC::Base::replacements_t reps; // in 3.9, this is std::set
   vec_str ftargs = {"f","g","h"};
-  string_t new_arg("boo");
-  EC ec(reps,ftargs,new_arg,false);
+  replacement_t er1(fname,29,0,boo);
+  replacement_t er2(fname,82,0,boo);
+  replacement_t er3(fname,106,0,boo);
+  expand_callsite ec(reps,ftargs,new_arg,false);
+  EC::Base::replacements_t exp_reps = {er1,er2,er3};
+
+  ASTUPtr ast; ASTContext * pctx; TranslationUnitDecl * decl;
+  std::tie(ast, pctx, decl) = prep_code(code);
   auto ms(ec.matchers());
   finder_t finder;
   for(auto & m: ms){
     finder.addMatcher(m, &ec);
   }
-  finder.addMatcher(
-      mk_mthd_call_matcher(EC::cs_bind_name_, EC::fn_bind_name_, "h"), &ec);
+
+  string_t const mthd = "h";
+  auto mthd_matcher =
+    mk_mthd_call_matcher(EC::cs_bind_name_,EC::fn_bind_name_,mthd);
+  finder.addMatcher(mthd_matcher,&ec);
+
   finder.matchAST(*pctx);
-  replacement_t er1("input.cc",29,0,"boo");
-  replacement_t er2("input.cc",82,0,"boo");
-  replacement_t er3("input.cc",106,0,"boo");
-  EC::Base::replacements_t exp_reps = {er1,er2,er3};
   EXPECT_EQ(exp_reps.size(),reps.size());
-  using it_t = expand_callsite::Base::replacements_t::iterator;
-  it_t ri = reps.begin(),ei = exp_reps.begin();
-  for(; ri != reps.end() && ei != exp_reps.end(); ++ri,++ei){
-    bool const rep_ok = *ri == *ei;
-    if(!rep_ok){
-      std::cout << "rep: " << ri->toString() << ", expected: " << ei->toString()
-        << std::endl;
-    }
-    EXPECT_TRUE(rep_ok);
-  }
+  EXPECT_EQ(exp_reps,reps);
 } // TEST(expand_callsite,expands)
 
 
