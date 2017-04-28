e99a411a7f5eae6b357e62543eaab153a516efc7
Timothy Kelley <tkelley@lanl.gov>
2017-04-28 16:12:50 -0600

    Added another example

--- test/lib/test_input_fn_sig.cpp
+++ test/lib/test_input_fn_sig.cpp
@@ -4,8 +4,10 @@
 // (c) Copyright 2017 LANSLLC, all rights reserved
 
 void f(int i, double d){}
+void fc(int i, const double d){}
 void h(int i, double& d){}
 void k(int i, double const & d){}
 
+double m(volatile int x, const volatile double y){return double();}
 
 // End of file
