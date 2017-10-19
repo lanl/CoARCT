d52de4e53be752e6b56f9af201492526da92b166
Timothy Kelley <tkelley@lanl.gov>
2017-10-19 11:59:36 -0600

    Added some more function signatures.

--- test/lib/test_input_fn_sig.cpp
+++ test/lib/test_input_fn_sig.cpp
@@ -5,9 +5,11 @@
 
 void f(int i, double d){}
 void fc(int i, const double d){}
+void fc2(int i, double const d){}
 void h(int i, double& d){}
 void k(int i, double const & d){}
 
 double m(volatile int x, const volatile double y){return double();}
+double m2(volatile int x, volatile const double y){return double();}
 
 // End of file
