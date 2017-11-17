dccc59019546c21cfd154afbfd6b076c21708f14
Timothy Kelley <tkelley@lanl.gov>
2017-11-16 17:30:09 -0700

    Added matchers/callback, tests for template variable declarations.

---
+++ lib/template_var_matchers.h
@@ -0,0 +1,127 @@
+// template_var_matchers.h
+// Nov 15, 2017
+// (c) Copyright 2017 LANSLLC, all rights reserved
+
+#pragma once
+
+#include "dump_things.h"
+#include "make_replacement.h"
+#include "types.h"
+#include "utilities.h"
+
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/Core/Replacement.h"
+#include "clang/Tooling/Tooling.h"
+#include <iostream>
+#include <map>
+#include <set>
+#include <sstream>
+#include <vector>
+
+namespace corct {
+
+// clang-format off
+clang::ast_matchers::DeclarationMatcher
+mk_templ_var_matcher(str_t_cr template_name){
+  using namespace clang::ast_matchers;
+  return
+    varDecl(
+      hasType(
+        classTemplateSpecializationDecl(
+           matchesName(template_name)
+        ).bind("mtvm_classDecl") // classTemplateSpecializationDecl
+      ) // hasType
+    ).bind("mtvm_varDecl"); // varDecl
+} // mk_templ_var_matcher
+
+auto
+mk_templ_var_matcher(str_t_cr template_name, str_t_cr namespace_name)
+{
+  using namespace clang::ast_matchers;
+  return
+    varDecl(
+      hasType(
+        classTemplateSpecializationDecl(
+          matchesName(template_name)
+         ,hasDeclContext(
+            namespaceDecl(
+              hasName(namespace_name)
+            ) // namespaceDecl
+          ) // hasDeclContext
+        ).bind("mtvm_classDecl") // classTemplateSpecializationDecl
+      ) // hasType
+    ).bind("mtvm_varDecl"); // varDecl
+} // mk_templ_var_matcher
+// clang-format on
+
+/**\brief Find instances of a template variable and record the names of the
+ * template arguments. */
+struct template_var_reporter : public callback_t {
+  using vec_strs_t = std::vector<string_t>;
+  using map_args_t =
+      std::map<string_t /*var name*/, vec_strs_t /*template args*/>;
+  using matcher_t = clang::ast_matchers::DeclarationMatcher;
+  using matchers_t = std::vector<matcher_t>;
+
+  matchers_t matchers() const
+  {
+    matchers_t ms;
+    if(!namespace_name_.empty()) {
+      ms.push_back(mk_templ_var_matcher(template_name_, namespace_name_));
+    }
+    else {
+      ms.push_back(mk_templ_var_matcher(template_name_));
+    }
+    return ms;
+  }  // matchers
+
+  virtual void run(const result_t & result) override
+  {
+    using namespace clang;
+    // ASTContext & ctx(*(result.Context));
+    ClassTemplateSpecializationDecl const * c_decl =
+        result.Nodes.getNodeAs<ClassTemplateSpecializationDecl>(
+            "mtvm_classDecl");
+    VarDecl const * v_decl = result.Nodes.getNodeAs<VarDecl>("mtvm_varDecl");
+    if(c_decl && v_decl) {
+      string_t var_name = v_decl->getQualifiedNameAsString();
+      auto & args_list(c_decl->getTemplateArgs());
+      uint32_t n_args = args_list.size();
+      vec_strs_t arg_names;
+      for(uint32_t i = 0; i < n_args; ++i) {
+        TemplateArgument const & ta(args_list.get(i));
+        bool is_it_a_pack(ta.isPackExpansion());
+        if(is_it_a_pack) {
+          // hmmm...
+          printf("%s:%i Found a template pack arg, not sure what to do\n",
+                 __FUNCTION__, __LINE__);
+        }
+        else {
+          QualType qtype(ta.getAsType());
+          string_t qtype_name(qtype.getAsString());
+          arg_names.push_back(qtype_name);
+        }
+      }  // for(arg:args)
+      args_[var_name] = std::move(arg_names);
+    }
+    else {
+      check_ptr(c_decl, "classTemplateSpecializationDecl");
+      check_ptr(v_decl, "varDecl");
+    }
+    return;
+  }  // run
+
+  template_var_reporter(str_t_cr template_name, str_t_cr namespace_name = "")
+      : template_name_(template_name), namespace_name_(namespace_name)
+  {
+  }
+
+  map_args_t args_;
+  string_t template_name_;
+  string_t namespace_name_;
+};  // template_var_reporter
+
+}  // corct::
+
+// End of file
---
+++ test/lib/template_var_matchers_test.cc
@@ -0,0 +1,248 @@
+// template_var_matchers_test.cc
+// Jan 05, 2017
+// (c) Copyright 2017 LANSLLC, all rights reserved
+
+/**\file Unit tests of template variable matchers*/
+
+#include "template_var_matchers.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Tooling/Refactoring.h"
+#include "clang/Tooling/Tooling.h"
+#include "gtest/gtest.h"
+#include "match_count.h"
+#include "prep_code.h"
+// #include "utilities.h"
+#include <vector>
+
+using namespace corct;
+using namespace corct_test;
+using namespace clang;
+using namespace clang::ast_matchers;
+
+/* Test strategy: write code including element to be tested, such as member
+expression; compile to AST; count number of times matcher hits nodes. It's a lot
+of work to get around constructing a consistent AST subtree directly.
+*/
+
+struct Tests_templ_match : public callback_t {
+  using matcher_t = DeclarationMatcher;
+
+  matcher_t matcher() const
+  {
+    if(namespace_name != "") {
+      return mk_templ_var_matcher(template_name, namespace_name);
+    }
+    return mk_templ_var_matcher(template_name);
+  }
+
+  void run(result_t const & result) override
+  {
+    matched_++;
+    return;
+  }  // run
+
+  string_t template_name = "";
+  string_t namespace_name = "";
+  uint32_t matched_ = 0;
+};  // Tests_ptr_ref
+
+TEST(template_var_matchers, template_name)
+{
+  {
+    string_t code = "template <class T> class Aardvarks{}; Aardvarks<int> a;";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 1u);
+  }
+  {
+    string_t code = "template <class T> class Bardvarks{}; Bardvarks<int> a;";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 0u);
+  }
+  {
+    string_t code =
+        "template <class T> class Aardvarks{};"
+        "Aardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> class Aardvarks{};"
+        "  Aardvarks<double> a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 5u);
+  }
+}  // TEST(template_var_matchers,named_template)
+
+TEST(template_var_matchers, namespaced_template)
+{
+  {
+    string_t code =
+        "template <class T> class Aardvarks{};"
+        "Aardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> class Aardvarks{};"
+        "  Aardvarks<double> a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    tester.namespace_name = "ns";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 1u);
+  }
+}  // TEST(template_var_matchers,namespaced_template)
+
+TEST(template_var_matchers, typedefd_template)
+{
+  /* The matcher "sees through" typedefs: */
+  {
+    string_t code =
+        "template <class T> class Aardvarks{};"
+        "using Cardvarks = Aardvarks<int>;"
+        "Cardvarks a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> class Aardvarks{};"
+        "  Aardvarks<double> a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 5u);
+  }
+  /* Same thing. Note that his generates a C++11 warning. */
+  {
+    string_t code =
+        "template <class T> class Aardvarks{};"
+        "template <class U> using Cardvarks = Aardvarks<U>;"
+        "Cardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> class Aardvarks{};"
+        "  Aardvarks<double> a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 5u);
+  }
+  /* The matcher "sees through" typedefs, with namespaces: */
+  {
+    string_t code =
+        "template <class T> class Aardvarks{};"
+        "using Cardvarks = Aardvarks<int>;"
+        "Cardvarks a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> class Aardvarks{};"
+        "  using Cardvarks = Aardvarks<int>;"
+        "  Cardvarks a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    tester.namespace_name = "ns";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 1u);
+  }
+  /* Same thing. Note that his generates a C++11 warning. */
+  {
+    string_t code =
+        "template <class T> struct Aardvarks{};"
+        "template <class U> using Cardvarks = Aardvarks<U>;"
+        "Cardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> struct Aardvarks{};"
+        "  template <class U> using Cardvarks = Aardvarks<U>;"
+        "  Cardvarks<double> a;"
+        "}";
+    Tests_templ_match tester;
+    tester.template_name = "Aardvarks";
+    tester.namespace_name = "ns";
+    uint32_t n_matches = count_matches<Tests_templ_match>(code, tester);
+    EXPECT_EQ(n_matches, 1u);
+  }
+
+}  // TEST(template_var_matchers,typedefd_template){
+
+TEST(template_var_matchers, template_var_reporter)
+{
+  using namespace clang;
+  using vec_strs_t = template_var_reporter::vec_strs_t;
+  {
+    string_t code =
+        "template <class T> struct Aardvarks{};"
+        "template <class U> using Cardvarks = Aardvarks<U>;"
+        "Cardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> struct Aardvarks{};"
+        "  template <class U> using Cardvarks = Aardvarks<U>;"
+        "  Cardvarks<double> a;"
+        "}";
+    template_var_reporter repo("Aardvarks");
+    ASTUPtr ast;
+    ASTContext * pctx;
+    TranslationUnitDecl * decl;
+    std::tie(ast, pctx, decl) = prep_code(code);
+    template_var_reporter::matchers_t ms(repo.matchers());
+    corct::finder_t finder;
+    for(auto m : ms) {
+      finder.addMatcher(m, &repo);
+    }
+    finder.matchAST(*pctx);
+    auto & args(repo.args_);
+    EXPECT_EQ(args.size(), 5);
+    vec_strs_t exp_keys = {"a", "b", "c", "d", "ns::a"};
+    vec_strs_t exp_vals[5] = {
+        {"int"}, {"float"}, {"float"}, {"float"}, {"double"}};
+    for(size_t i = 0; i < args.size() && i < exp_keys.size(); ++i) {
+      string_t & var_name = exp_keys[i];
+      EXPECT_EQ(args.count(var_name), 1u);
+      vec_strs_t & val = exp_vals[i];
+      EXPECT_EQ(args[var_name], val);
+    }
+  }
+  /* Same as above, but restricted to namespace*/
+  {
+    string_t code =
+        "template <class T> struct Aardvarks{};"
+        "template <class U> using Cardvarks = Aardvarks<U>;"
+        "Cardvarks<int> a;"
+        "Aardvarks<float> b,c,d;"
+        "namespace ns{"
+        "  template <class T> struct Aardvarks{};"
+        "  template <class U> using Cardvarks = Aardvarks<U>;"
+        "  Cardvarks<double> a;"
+        "}";
+    template_var_reporter repo("Aardvarks", "ns");
+    ASTUPtr ast;
+    ASTContext * pctx;
+    TranslationUnitDecl * decl;
+    std::tie(ast, pctx, decl) = prep_code(code);
+    template_var_reporter::matchers_t ms(repo.matchers());
+    corct::finder_t finder;
+    for(auto m : ms) {
+      finder.addMatcher(m, &repo);
+    }
+    finder.matchAST(*pctx);
+    auto & args(repo.args_);
+    EXPECT_EQ(args.size(), 1);
+    vec_strs_t exp_keys = {"ns::a"};
+    vec_strs_t exp_vals[5] = {{"double"}};
+    for(size_t i = 0; i < args.size() && i < exp_keys.size(); ++i) {
+      string_t & var_name = exp_keys[i];
+      EXPECT_EQ(args.count(var_name), 1u);
+      vec_strs_t & val = exp_vals[i];
+      EXPECT_EQ(args[var_name], val);
+    }
+  }
+
+}  // TEST(template_var_matchers,template_var_reporter)
+
+// End of file
