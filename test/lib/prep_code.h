ccd937962ca3dafacd321d390f172c7fb5f006b8
Timothy Kelley <tkelley@lanl.gov>
2018-02-02 16:45:16 -0700

    Formatting

--- test/lib/prep_code.h
+++ test/lib/prep_code.h
@@ -14,13 +14,15 @@
 using ASTUPtr = std::unique_ptr<clang::ASTUnit>;
 
 /**\brief Compile code fragment to AST.
-\param code: valid c++ code
-\return {unique_ptr<AST>,ASTContext *, TranslationDecl *}
-*/
+ * \param code: valid c++ code
+ * \return {unique_ptr<AST>,ASTContext *, TranslationDecl *}
+ *
+ * Will provide -std=c++14 and  Clang include directories as compiler flags.
+ */
 inline auto
 prep_code(corct::str_t_cr code)
 {
-  corct::vec_str args = {"-std=c++14", corct::clang_inc_dir1,
+  corct::vec_str args = {"-std=c++14",corct::clang_inc_dir1,
                          corct::clang_inc_dir2};
   ASTUPtr ast(clang::tooling::buildASTFromCodeWithArgs(code,args));
   clang::ASTContext * pctx = &(ast->getASTContext());
