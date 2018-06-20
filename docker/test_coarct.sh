07d3184aa86fb2101b8d106ff7fb54992d4c3edd
Timothy Kelley <tkelley@lanl.gov>
2018-06-20 10:10:36 -0600

    Adding docker files.

---
+++ docker/test_coarct.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+ git clone https://github.com/lanl/CoARCT.git -b clang-6.0 coarct
+ cd coarct
+ mkdir build
+ cd build
+ cmake ..
+ make VERBOSE=on -j 4 2>&1 | tee make.out
+ ./test/corct_unittests
